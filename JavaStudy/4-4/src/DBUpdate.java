a313441b96c7c8c8256ce58005a705f1e8b52af1
Moka-131 <114382191+Moka-131@users.noreply.github.com>
2023-04-28 18:05:31 +0900

    4-4課題提出

--- JavaStudy/4-4/src/DBUpdate.java
+++ JavaStudy/4-4/src/DBUpdate.java
@@ -45,10 +45,10 @@ public class DBUpdate {
             statement = connection.createStatement();
 
             // 問⑤ SHOHIN_IDが020のSHOHIN_NAMEを「商品20」に変更するためのSQL文を記述しましょう。
-            String SQL = "UPDATE TB_SHOHIN SET SHOHIN_NAME = '商品20' WHERE SHOHIN_ID = 020";
+            String SQL = "UPDATE TB_SHOHIN SET SHOHIN_NAME = '商品20' WHERE SHOHIN_ID = '020'";
 
             // 問⑥ 上記のSQL文を実行するための文を記述しましょう。
-            resultSet = statement.executeQuery(SQL);
+            statement.executeUpdate(SQL);
             //一覧表示
             String SQLselect = "SELECT * FROM TB_SHOHIN";
             resultSet = statement.executeQuery(SQLselect);
